82e53da059e0a3e54848759e4af695c5ea91505e
Johan-Gutierrez <161645881+Johan-Gutierrez@users.noreply.github.com>
2024-05-16 11:49:44 -0600

    proyecto inicial

---
+++ monitor/js/main.js
@@ -0,0 +1,27 @@
+
+let respuesta = document.getElementById('respuesta');
+
+function callApiRequest(){
+
+    // Hacer una petición para un usuario con ID especifico
+axios.get('http://localhost/iot-car-control/back-end/apis/getRegistro.php')
+.then(function (response) {
+  // manejar respuesta exitosa
+  console.log(response);
+
+  let respuestaservidor = response.data == "a"?"ADELANTE":response.data == "r"?"ATRAS":response.data == "i"?"IZQUIERDA":response.data == "d"?"DERECHA": response.data == "p"?"PARAR":"LETRA NO CONOCIDA";
+  respuesta.innerHTML = "Respuesta: <strong>" +respuestaservidor;
+})
+.catch(function (error) {
+  // manejar error
+  console.log(error);
+})
+.finally(function () {
+  // siempre sera executado
+});
+}
+
+// Repite la función cada 2 segundos
+setInterval(callApiRequest, 2000);
+
+
